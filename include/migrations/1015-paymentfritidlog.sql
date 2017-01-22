a637101231c6ff68381bf7555e147884e9ceca38
Peter Lind <peter.e.lind@gmail.com>
2017-01-22 11:04:10 +0100

    updating broken migration

--- include/migrations/1015-paymentfritidlog.sql
+++ include/migrations/1015-paymentfritidlog.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS paymentfritidlog;
 CREATE TABLE paymentfritidlog (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    participant_id INT(11) NOT NULL PRIMARY KEY,
+    participant_id INT(11) NOT NULL,
     amount INT UNSIGNED NOT NULL,
     cost INT UNSIGNED NOT NULL,
     fees INT UNSIGNED NOT NULL,
