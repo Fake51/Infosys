2029364efc09acc9bf3baa4ad49a0ffef86f8ed1
Peter Lind <peter.e.lind@gmail.com>
2017-02-21 23:19:53 +0100

    fixing flaw in model of participant templates

--- include/migrations/1019-participant-templates.sql
+++ include/migrations/1019-participant-templates.sql
@@ -1,9 +1,8 @@
 DROP TABLE IF EXISTS participantidtemplates;
 
 CREATE TABLE participantidtemplates (
+    participant_id INT NOT NULL PRIMARY KEY,
     template_id INT UNSIGNED NOT NULL,
-    participant_id INT NOT NULL,
-    PRIMARY KEY template_participant (template_id, participant_id),
     CONSTRAINT FOREIGN KEY participant (participant_id) REFERENCES deltagere (id) ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT FOREIGN KEY template (template_id) REFERENCES idtemplates (id) ON UPDATE CASCADE ON DELETE CASCADE
 ) engine=InnoDB character set utf8;
