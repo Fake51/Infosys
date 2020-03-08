d75ed2eafcafe9d46d6640313b978c18a0d66a4c
Peter Lind <peter.e.lind@gmail.com>
2020-03-08 21:35:49 +0100

    adding hwi oauth

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
